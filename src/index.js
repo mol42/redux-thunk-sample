af30508dcad7e1837f66e2a87e7d2cb826fcf737
Tayfun YÜĞRÜK <tayfunyugruk@gmail.com>
2019-02-06 00:57:16 -0500

    redux thunk comments are added

--- src/index.js
+++ src/index.js
@@ -4,7 +4,14 @@ import AppContainer from './App';
 import './index.css';
 
 // Add these imports - Step 1
+// Provider is a helper class which helps
+// connect function to find the global
+// store easily
+// Provider is a Context actually.
 import { Provider } from 'react-redux';
+// we use the store that we create in redux.js file
+// and pass it to Provider class so that it
+// becomes visible to whole React tree.
 import { store } from './redux';
 
 // Wrap existing app in Provider - Step 2
--- src/redux.js
+++ src/redux.js
@@ -2,14 +2,24 @@ import { applyMiddleware, combineReducers, createStore } from 'redux';
 
 import thunk from 'redux-thunk';
 
-// actions.js
+// action generator function, which is basically creates 
+// an action object and returns it normally we do not have to
+// use action generators but it helps us to craeate a utility like
+// mechanism to create actions
 export const addRepos = repos => ({
   type: 'ADD_REPOS',
   repos,
 });
 
+// action generator function, which is basically creates 
+// an action object and returns it normally we do not have to
+// use action generators but it helps us to craeate a utility like
+// mechanism to create actions
 export const clearRepos = () => ({ type: 'CLEAR_REPOS' });
 
+// this function is captured and invoked by redux-thunk
+// normally redux does not call functions it only accept
+// action objects and it works synhroniously
 export const getRepos = username => async dispatch => {
   try {
     const url = `https://api.github.com/users/${username}/repos?sort=updated`;
@@ -22,7 +32,9 @@ export const getRepos = username => async dispatch => {
   }
 };
 
-// reducers.js
+// reducer is basically a function which accepts two
+// parameters. first parameter is current state second
+// parameter is redux action
 export const repos = (state = [], action) => {
   switch (action.type) {
     case 'ADD_REPOS':
@@ -34,10 +46,18 @@ export const repos = (state = [], action) => {
   }
 };
 
+// reducers are combined here, the idea with combining reducers is to
+// seperate logically connected data together and in their own file.
+// for ex : 
+//     - user
+//     - colleges
+//     - messaging
 export const reducers = combineReducers({ repos });
 
-// store.js
+// redux store (global data managemer) is created here.
 export function configureStore(initialState = {}) {
+  // here is important to enable thunk library we register it
+  // as middleware while creating the store
   const store = createStore(reducers, initialState, applyMiddleware(thunk));
   return store;
 }
